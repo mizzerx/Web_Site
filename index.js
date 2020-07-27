f2bf0847e6ff18ffec1934a0c5a1a15facf15e73
mizzerx <vuhunglcute@gmail.com>
2020-07-27 17:02:33 +0700

    Change to live port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const engine = require('consolidate');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 5000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
